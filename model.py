a8f1bf46225f7f8eafe9fe1b383925fee2bb0c56
Lala Arnav Vatsal <lalaarnav003@gmail.com>
2024-04-17 17:54:33 +0530

    Added script to train model.

---
+++ model.py
@@ -0,0 +1,238 @@
+import cv2
+import tensorflow as tf
+import json
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def load_image(x):
+    byte_img = tf.io.read_file(x)
+    img = tf.io.decode_jpeg(byte_img)
+    return img
+
+
+def load_labels(label_path):
+    with open(label_path.numpy(), 'r', encoding="utf-8") as f:
+        label = json.load(f)
+
+    return [label['class']], label['bbox']
+
+
+train_images = tf.data.Dataset.list_files('aug_data\\train\\images\\*.jpg', shuffle=False)
+train_images = train_images.map(load_image)
+train_images = train_images.map(lambda x: tf.image.resize(x, (120, 120)))
+train_images = train_images.map(lambda x: x / 255)
+
+print("Train images loaded.")
+
+train_labels = tf.data.Dataset.list_files('aug_data\\train\\labels\\*.json', shuffle=False)
+train_labels = train_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
+
+print("Train image labels loaded.")
+
+train = tf.data.Dataset.zip((train_images, train_labels))
+train = train.shuffle(5000)
+train = train.batch(8)
+train = train.prefetch(4)
+
+print("Train dataset created.")
+
+test_images = tf.data.Dataset.list_files('aug_data\\test\\images\\*.jpg', shuffle=False)
+test_images = test_images.map(load_image)
+test_images = test_images.map(lambda x: tf.image.resize(x, (120, 120)))
+test_images = test_images.map(lambda x: x / 255)
+
+print("Test images loaded.")
+
+test_labels = tf.data.Dataset.list_files('aug_data\\test\\labels\\*.json', shuffle=False)
+test_labels = test_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
+
+print("Test image labels loaded.")
+
+test = tf.data.Dataset.zip((test_images, test_labels))
+test = test.shuffle(1300)
+test = test.batch(8)
+test = test.prefetch(4)
+
+print("Test dataset created.")
+
+val_images = tf.data.Dataset.list_files('aug_data\\val\\images\\*.jpg', shuffle=False)
+val_images = val_images.map(load_image)
+val_images = val_images.map(lambda x: tf.image.resize(x, (120, 120)))
+val_images = val_images.map(lambda x: x / 255)
+
+print("Val images loaded.")
+
+val_labels = tf.data.Dataset.list_files('aug_data\\val\\labels\\*.json', shuffle=False)
+val_labels = val_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
+
+print("Val image labels loaded.")
+
+val = tf.data.Dataset.zip((val_images, val_labels))
+val = val.shuffle(1000)
+val = val.batch(8)
+val = val.prefetch(4)
+
+print("Val dataset created.")
+
+# data_samples = train.as_numpy_iterator()
+# res = data_samples.next()
+# fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+# for idx in range(4):
+#     sample_image = res[0][idx]
+#     sample_coords = res[1][1][idx]
+#
+#     cv2.rectangle(sample_image,
+#                   tuple(np.multiply(sample_coords[:2], [120, 120]).astype(int)),
+#                   tuple(np.multiply(sample_coords[2:], [120, 120]).astype(int)),
+#                   (255, 0, 0), 2)
+#
+#     ax[idx].imshow(sample_image)
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Dense, GlobalMaxPooling2D
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.callbacks import TensorBoard
+
+vgg = VGG16(include_top=False)
+print(vgg.summary())
+
+
+def build_model():
+    input_layer = Input(shape=(120, 120, 3))
+    vgg = VGG16(include_top=False)(input_layer)
+
+    # Classification Model
+    f1 = GlobalMaxPooling2D()(vgg)
+    class1 = Dense(2048, activation='relu')(f1)
+    class2 = Dense(1, activation='sigmoid')(class1)
+
+    # Bounding box model
+    f2 = GlobalMaxPooling2D()(vgg)
+    regress1 = Dense(2048, activation='relu')(f2)
+    regress2 = Dense(4, activation='sigmoid')(regress1)
+
+    facetracker = Model(inputs=input_layer, outputs=[class2, regress2])
+    return facetracker
+
+
+facetracker = build_model()
+print(facetracker.summary())
+
+batches_per_epoch = len(train)
+lr_decay = (1 / 3) / batches_per_epoch
+lr_schedule = ExponentialDecay(
+    initial_learning_rate=0.0001,
+    decay_steps=10000,
+    decay_rate=lr_decay)
+opt = Adam(learning_rate=lr_schedule)
+
+
+def localization_loss(y_true, yhat):
+    delta_coord = tf.reduce_sum(tf.square(y_true[:, :2] - yhat[:, :2]))
+
+    h_true = y_true[:, 3] - y_true[:, 1]
+    w_true = y_true[:, 2] - y_true[:, 0]
+
+    h_pred = yhat[:, 3] - yhat[:, 1]
+    w_pred = yhat[:, 2] - yhat[:, 0]
+
+    delta_size = tf.reduce_sum(tf.square(w_true - w_pred) + tf.square(h_true - h_pred))
+
+    return delta_coord + delta_size
+
+
+classloss = BinaryCrossentropy()
+regressloss = localization_loss
+
+print("Testing model without training.")
+
+X, y = train.as_numpy_iterator().next()
+print(f"Batch size: {X.shape[0]}")
+print(f"Shape of X: {X[0].shape}")
+
+classes, coords = facetracker.predict(X)
+for i in range(len(classes)):
+    print(f"Predicted class: {classes[i]}, Actual class: {y[0][i]}")
+    print(f"Predicted coordinates: {coords[i]}, Actual coordinates: {y[1][i]}")
+
+print(f"Localization Loss: {regressloss(y[1], coords)}")
+print(f"Class Loss: {classloss(y[0], classes)}")
+
+
+class FaceTracker(Model):
+    def __init__(self, model, **kwargs):
+        super().__init__(**kwargs)
+        self.model = model
+
+    def compile(self, opt, classloss, localizationloss, **kwargs):
+        super().compile(**kwargs)
+        self.closs = classloss
+        self.lloss = localizationloss
+        self.opt = opt
+
+    def train_step(self, batch, **kwargs):
+        X, y = batch
+
+        with tf.GradientTape() as tape:
+            classes, coords = self.model(X, training=True)
+
+            batch_classloss = self.closs(tf.cast(tf.reshape(y[0], (-1, 1)), tf.float32), classes)
+            batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+
+            total_loss = batch_localizationloss + 0.5 * batch_classloss
+
+            grad = tape.gradient(total_loss, self.model.trainable_variables)
+
+        opt.apply_gradients(zip(grad, self.model.trainable_variables))
+
+        return {"total_loss": total_loss, "class_loss": batch_classloss, "regress_loss": batch_localizationloss}
+
+    def test_step(self, batch, **kwargs):
+        X, y = batch
+
+        classes, coords = self.model(X, training=False)
+
+        batch_classloss = self.closs(tf.cast(tf.reshape(y[0], (-1, 1)), tf.float32), classes)
+        batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+        total_loss = batch_localizationloss + 0.5 * batch_classloss
+
+        return {"total_loss": total_loss, "class_loss": batch_classloss, "regress_loss": batch_localizationloss}
+
+    def call(self, X, **kwargs):
+        return self.model(X, **kwargs)
+
+
+model = FaceTracker(facetracker)
+model.compile(opt, classloss, regressloss)
+
+logdir = 'logs'
+tensorboard_callback = TensorBoard(log_dir=logdir)
+
+hist = model.fit(train, epochs=10, validation_data=val, callbacks=[tensorboard_callback])
+
+model.save('face_tracker.keras')
+
+print(hist.history)
+
+fig, ax = plt.subplots(ncols=3, figsize=(20, 5))
+
+ax[0].plot(hist.history['total_loss'], color='teal', label='loss')
+ax[0].plot(hist.history['val_total_loss'], color='orange', label='val loss')
+ax[0].title.set_text('Loss')
+ax[0].legend()
+
+ax[1].plot(hist.history['class_loss'], color='teal', label='class loss')
+ax[1].plot(hist.history['val_class_loss'], color='orange', label='val class loss')
+ax[1].title.set_text('Classification Loss')
+ax[1].legend()
+
+ax[2].plot(hist.history['regress_loss'], color='teal', label='regress loss')
+ax[2].plot(hist.history['val_regress_loss'], color='orange', label='val regress loss')
+ax[2].title.set_text('Regression Loss')
+ax[2].legend()
+
+plt.show()
